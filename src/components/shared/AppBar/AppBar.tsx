191c86f53560f6c0037a9d4f0e9f56fa7413a955
OlgaPavlenko <olgaigpavlenko@gmail.com>
2024-08-05 13:45:00 +0100

    deleted Outlet from AppBar

--- src/components/shared/AppBar/AppBar.tsx
+++ src/components/shared/AppBar/AppBar.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 
-import { Link, Outlet } from "react-router-dom";
-
 import AdbIcon from "@mui/icons-material/Adb";
 import AppBar from "@mui/material/AppBar";
 import Avatar from "@mui/material/Avatar";
@@ -9,6 +7,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
